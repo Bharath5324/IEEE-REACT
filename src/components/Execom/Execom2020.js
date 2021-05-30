ab9b42998c8db25713fe64843f2a41ed807bb182
Sreehari N <sree1001hari@gmail.com>
2021-05-31 01:26:40 +0530

    changes committed

---
+++ src/components/Execom/Execom2020.js
@@ -0,0 +1,196 @@
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import './Execom.css';
+const Execom20 = () =>{
+    var pad = {
+    }
+    // var Heading = {
+    //     "font-size": "40 px"
+    // }
+    return(
+        <div className="container" >
+        <div className="row light-blue-text text-darken-4">
+        <div>
+        <div style={{ "textAlign": "center", "paddingBottom": "100px", "paddingRight": "50px", "paddingLeft": "50px"}} className="">
+            <h1>Student Branch</h1>
+
+            <div class="divider"></div>
+        </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/Christina.jpg")} alt="Chair" />
+                <h5>Christina Ann Zachariah</h5>
+                <p> Chairperson</p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/VC.jpg")} alt=" Vice Chair" />
+                <h5>Anuvinda Krishnan</h5>
+                <p> Vice Chairperson</p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/Secretary.jpg")} alt="Secretary" />
+                <h5>Anand Raj</h5>
+                <p> Secretary</p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/gokul.jpg")} alt="Treasurer" />
+                <h5>Gokul</h5>
+                <p> Treasurer</p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/Link_rep.jpg")} alt="Link Representative" />
+                <h5>Amith Tony Joseph</h5>
+                <p> Link Representative</p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/MDC.jpg")} alt="Membership Development Ccoordinator" />
+                <h5>Vinu</h5>
+                <p> Membership Development Coorinator</p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/AC.jpg")} alt="Activities Coordinator" />
+                <h5>Sreehari</h5>
+                <p> Activity Coordinator</p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250pxx", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/pp.jpg")} alt="Design Head" />
+                <h5>Adil Rafeek</h5>
+                <p> Design Head</p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250pxx", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/Content_head.jpg")} alt="Content Head" />
+                <h5>Samyuktha</h5>
+                <p> Content Head</p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/TC.jpg")} alt="Technical Coordinator" />
+                <h5>Amaya Sam</h5>
+                <p> Technical Coordinator</p>
+            </div>
+        </div>
+        </div>
+        <div className="row light-blue-text text-darken-4">
+        <div style={{ "text-align": "center", "padding-bottom": "100px", "padding-left": "20px", "padding-right": "40px"}}>
+            <h2>Computer Society</h2>
+            <div class="divider"></div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/CS_chair.jpg")} alt="CS Chair" />
+                <h5>Malavika S Menon</h5>
+                <p> Chairperson</p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4 ">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/CS_secretary.jpg")} alt="CS Secretatry" />
+                <h5>Bharath Srinivasan</h5>
+                <p> Secretary</p>
+            </div>
+        </div>
+        </div>
+        <div className="row light-blue-text text-darken-4">
+        <div style={{"text-align": "center", "padding-top": "100px", "padding-bottom": "100px", "padding-left": "20px", "padding-right": "40px"}}>
+            <h2>Signal Processing Society</h2>
+            <div class="divider"></div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/SPS_chair.jpg")} alt="SPS Chair" />
+                <h5>Shyama</h5>
+                <p> Chairperson</p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/SPS_secretary.jpg")} alt="SPS Secretary" />
+                <h5>Mukund C Menon</h5>
+                <p> Secretary</p>
+            </div>
+        </div>
+        </div>
+        <div className="row light-blue-text text-darken-4">
+        <div style={{"text-align": "center", "padding-top": "100px", "padding-bottom": "100px", "padding-left": "20px", "padding-right": "40px"}}>
+            <h2>Industry Applications Society</h2>
+            <div class="divider"></div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/PES_chair.jpg")} alt="IAS Chair" />
+                <h5>Aparna</h5>
+                <p> Chairperson</p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/IAS_secretary.jpg")} alt="IAS Secretary" />
+                <h5>Serene</h5>
+                <p> Secretary</p>
+            </div>
+        </div>
+        </div>
+        <div className="row light-blue-text text-darken-4">
+        <div style={{"text-align": "center", "padding-top": "100px", "padding-bottom": "100px", "padding-left": "20px", "padding-right": "40px"}}>
+            <h2>Women in Engineering Affinity Group</h2>
+            <div class="divider"></div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/WiE_chair.jpg")} alt="WIE Chair" />
+                <h5>Poornima</h5>
+                <p> Chairperson</p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/WiE_secretary.jpg")} alt="WIE Secretary" />
+                <h5>Anjali Suresh</h5>
+                <p> Secretary</p>
+            </div>
+        </div>
+        </div>
+        <div className="row light-blue-text text-darken-4">
+        <div style={{"text-align": "center", "padding-top": "100px", "padding-bottom": "100px", "padding-left": "20px", "padding-right": "40px"}}>
+            <h2>Power & Energy Society</h2>
+            <div class="divider"></div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/PES_chair.jpg")} alt="PES Chair" />
+                <h5>Aparna</h5>
+                <p> Chairperson</p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos20/PES_secretary.jpg")} alt="PES Secretary" />
+                <h5>Gowri Lakshmi</h5>
+                <p> Secretary</p>
+            </div>
+        </div>
+        </div>
+        </div>
+
+    )
+}
+
+export default Execom20;
---
+++ src/components/Execom/Execom2021.js
@@ -0,0 +1,246 @@
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import './Execom.css';
+const Execom21 = () =>{
+    var pad = {
+    }
+    // var Heading = {
+    //     "font-size": "40 px"
+    // }
+    return(
+        <div className="container" >
+               <div class="row">
+                 <div class="col s12 m12 l12">
+             <div class="card grey lighten-2 z-depth-4">
+            <div class="card-content white-text">
+            <div className="row light-blue-text text-darken-4">
+        <div>
+        <div style={{ "textAlign": "center", "paddingBottom": "100px", "paddingRight": "50px", "paddingLeft": "50px"}} className="">
+            <h1>Student Branch</h1>
+            <div className="divider light-blue darken-4"></div>
+            <div class="divider"></div>
+        </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/SB_chair_Abhijith P.jpg")} alt="Chair" />
+                <h5>Abhijith P</h5>
+                <p><b> Chairperson </b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/ViceChairperson_Amith Tony Joseph.jpeg")} alt=" Vice Chair" />
+                <h5>Amit Tony Joseph</h5>
+                <p><b> Vice Chairperson</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/Secretary_Shivi Gupta.jpg")} alt="Secretary" />
+                <h5>Shivi Gupta</h5>
+                <p><b> Secretary</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/Treasurer_Anuvinda Krishnan V K.jpg")} alt="Treasurer" />
+                <h5>Anuvinda Krishnan</h5>
+                <p><b> Treasurer</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/LINK Representative_Amay Ganesh.jpg")} alt="Link Representative" />
+                <h5>Amay Ganesh</h5>
+                <p><b> Link Representative</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/MEMBERSHIP DEVELOPMENT COORDINATOR_DURGA K B.jpg")} alt="Membership Development Ccoordinator" />
+                <h5>Durga K B</h5>
+                <p><b> Membership Development Coordinator</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/Technical Coordinator_Sreehari N.jpg")} alt="Activities Coordinator" />
+                <h5>Sreehari N</h5>
+                <p><b>Technical Coordinator</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/Design head-Riya Lalu.JPG")} alt="Design Head" />
+                <h5>Riya Lalu</h5>
+                <p> <b>Design Head</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "259px"}} className="circle responsive-img" src={require("./Execom_Photos21/Content Manager-Broadcast_ Vishnumaya S Unni.jpg")} alt="Content Manager Broadcast" />
+                <h5>Vishnumaya S Unni</h5>
+                <p><b> Content Manager-Broadcast</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/Content Manager - Proposals_Gouri Krishna.jpg")} alt="Content Manager Proposals" />
+                <h5>Gouri Krishna</h5>
+                <p><b> Content Manager-Proposals</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/ECC_Madhav.jpg")} alt="ECC" />
+                <h5>Madhav</h5>
+                <p><b>ECC</b></p>
+            </div>
+        </div>
+        </div>
+        <div className="row light-blue-text text-darken-4">
+        <div style={{ "text-align": "center", "padding-bottom": "100px", "padding-left": "20px", "padding-right": "40px"}}>
+            <h2><b>Computer Society</b></h2>
+            <div class="divider"></div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/CS Chair_ Bharath Srinivasan.jpg")} alt="CS Chair" />
+                <h5>Bharath Srinivasan</h5>
+                <p> <b>Chairperson</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/CS Secretary_Mohammed Salman Ali Pary.jpg")} alt="CS Secretatry" />
+                <h5>Mohammed Salman Ali Pary</h5>
+                <p> <b>Secretary</b></p>
+            </div>
+        </div>
+         <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/CSActivityCoordi_AbhiramAmbadiS.jpg")} alt="CS Activity Coordinator" />
+                <h5>Abhiram Ambadi S</h5>
+                <p><b>Activity Coordinator</b></p>
+            </div>
+        </div>
+        </div>
+        <div className="row light-blue-text text-darken-4">
+        <div style={{"text-align": "center", "padding-top": "100px", "padding-bottom": "100px", "padding-left": "20px", "padding-right": "40px"}}>
+            <h2><b>Signal Processing Society</b></h2>
+            <div class="divider"></div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/SPS Chair_Mukund C Menon.jpg")} alt="SPS Chair" />
+                <h5>Mukund C Menon</h5>
+                <p><b> Chairperson</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/SPSSecretary_FrancoJoeM.jpg")} alt="SPS Secretary" />
+                <h5>Franko Joe</h5>
+                <p><b> Secretary</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/SPSActivityCoordinator_K V Akash.jpg")} alt="SPS Activity Coordinator" />
+                <h5>K V Akash</h5>
+                <p><b>Activity Coordinator</b></p>
+            </div>
+        </div>
+        </div>
+        <div className="row light-blue-text text-darken-4">
+        <div style={{"text-align": "center", "padding-top": "100px", "padding-bottom": "100px", "padding-left": "20px", "padding-right": "40px"}}>
+            <h2><b>Industry Applications Society</b></h2>
+            <div class="divider"></div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/IAS chair_Finny Philip Biju.jpg")} alt="IAS Chair" />
+                <h5>Finny Philip Biju</h5>
+                <p><b>Chairperson</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/IAS Secretary_Sanjali Sajith.jpg")} alt="IAS Secretary" />
+                <h5>Sanjali Sajith</h5>
+                <p><b>Secretary</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/IAS_Activity Cordinator-Allen Alex Abraham.jpg")} alt="IAS Activity Coordinator" />
+                <h5>Allen Alex Abraham</h5>
+                <p><b>Activity Coordinator</b></p>
+            </div>
+        </div>
+        </div>
+        <div className="row light-blue-text text-darken-4">
+        <div style={{"text-align": "center", "padding-top": "100px", "padding-bottom": "100px", "padding-left": "20px", "padding-right": "40px"}}>
+            <h2><b>Women in Engineering Affinity Group</b></h2>
+            <div class="divider"></div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/Wie Chairperson_Anjali Suresh.jpeg")} alt="WIE Chair" />
+                <h5>Anjali Suresh</h5>
+                <p><b> Chairperson</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/WIESecretary_RosiaJohny.jpg")} alt="WIE Secretary" />
+                <h5>Rosia Johny</h5>
+                <p><b> Secretary</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/WIE Activity coordinator_Kasinath V.jpg")} alt="WIE Activity Coordinator" />
+                <h5>Kasinath V</h5>
+                <p><b> Activity Coordinator</b></p>
+            </div>
+        </div>
+        </div>
+        <div className="row light-blue-text text-darken-4">
+        <div style={{"text-align": "center", "padding-top": "100px", "padding-bottom": "100px", "padding-left": "20px", "padding-right": "40px"}}>
+            <h2><b>Power & Energy Society</b></h2>
+            <div class="divider"></div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/PES Chairperson_AKSHAYRAJ K R.jpg")} alt="PES Chair" />
+                <h5>Akshayraj K R</h5>
+                <p> <b>Chairperson</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/PES Secretary_Shashank Nambiar.jpg")} alt="PES Secretary" />
+                <h5>Shashank Nambiar</h5>
+                <p><b> Secretary</b></p>
+            </div>
+        </div>
+        <div style={pad} className="col s12 m6 l4">
+            <div className="execom_img">
+                <img style={{"imageOrientation": "from-image", "height": "250px", "width": "250px"}} className="circle responsive-img" src={require("./Execom_Photos21/PES_ActivityCoordinator_AednaKurian.jpeg")} alt="PES Activity Coordinator" />
+                <h5>Aedna Kurian</h5>
+                <p><b>Activity Coordinator</b></p>
+            </div>
+            </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+
+    )
+}
+
+export default Execom21;
